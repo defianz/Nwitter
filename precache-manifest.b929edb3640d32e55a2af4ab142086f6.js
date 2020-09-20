1d222209799c38367c003e844dc658f5560bf479
Defian <iautm91@gmail.com>
2020-09-20 21:25:56 +0900

    Updates

---
+++ precache-manifest.b929edb3640d32e55a2af4ab142086f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3d8634a665f6707fa10807ceeb57cb",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "ab51f023f92ab2bc4a2d",
+    "url": "/nwitter/static/css/main.68e96ee5.chunk.css"
+  },
+  {
+    "revision": "ccc26fc361f8d854da5c",
+    "url": "/nwitter/static/js/2.fbda43ff.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.fbda43ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab51f023f92ab2bc4a2d",
+    "url": "/nwitter/static/js/main.3416b845.chunk.js"
+  },
+  {
+    "revision": "913a9c57646f983baf27",
+    "url": "/nwitter/static/js/runtime-main.dcdaaef8.js"
+  }
+]);
